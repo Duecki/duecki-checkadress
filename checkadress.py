1d96940e46b8618e6b7a3d9b2e6aa3df4e16b8d1
Duecki <frank@dueckinghaus.com>
2018-05-27 22:48:54 +0200

    SQL Sauber gezogen

--- checkadress.py
+++ checkadress.py
@@ -189,19 +189,11 @@ def mysqlcheck():
 
     format_str = """SELECT eintrag, longitude, latitude, date, city FROM TeslaLogDB.teslatracking where city is  NULL AND longitude <> 0 AND latitude <> 0 LIMIT {sqllimit};"""
     SQLQUERY = format_str.format(sqllimit=mysqllimit)
-
-    print SQLQUERY, type(SQLQUERY)
-
-
-#    SQLQUERY = str(SQLQUERY)
-#    print "SQLQUERY:",SQLQUERY,type(SQLQUERY)
     cursor.execute(SQLQUERY)
 
-    sys.exit(0)
     posts = mongoconnect()
-    cursor.execute("""SELECT eintrag, longitude, latitude, date, city FROM TeslaLogDB.teslatracking where city is  NULL AND longitude <> 0 AND latitude <> 0 LIMIT 2000""")
+#    cursor.execute("""SELECT eintrag, longitude, latitude, date, city FROM TeslaLogDB.teslatracking where city is  NULL AND longitude <> 0 AND latitude <> 0 LIMIT 2000""")
     for (eintrag, longitude, latitude, date, city) in cursor :
-
         geoatri={
             'longi':longitude,
             'lati':latitude,
